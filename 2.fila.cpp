45ea56ffadbfe53628349b1586edbdafaa4be4e0
Guilherme Gomes <guingomes@gmail.com>
2021-04-04 21:47:20 -0300

    Estudos e treinos

---
+++ 2.fila.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include "2.fila.h"
+
+using namespace std;
+
+    fila::fila() // construtor
+    {
+        primeiro = 0;
+        ultimo = 0;
+        estrutura = new TipoItem[max_itens];
+
+    }
+
+    fila::~fila() //destrutor
+    {
+        delete [] estrutura;
+    }
+
+    bool fila::estavazio()
+    {
+        return(primeiro == ultimo);
+    }
+
+    bool fila::estacheio()
+    {
+        return(ultimo-primeiro == max_itens);
+    }
+
+    void fila::inserir(TipoItem item)
+    {
+        if (estacheio()){
+            cout << "A fila esta cheia\n";
+            cout << "Esse elemento nao pode ser inserido.\n";
+        }else{
+            estrutura[ultimo % max_itens] = item;
+            ultimo++;
+        }
+    }
+
+    TipoItem fila::remover()
+    {
+        if (estavazio()){
+            cout << "A fila esta vazia.\n";
+            cout << "Nenhum elemento foi removido!\n";
+            return 0;
+        }else{
+            primeiro++;
+            return estrutura[primeiro-1 % max_itens];
+            
+        }
+    }
+
+    void fila::imprimir()
+    {
+        cout << "Fila: [ ";
+        for (int i=primeiro ; i<ultimo ; i++){
+            cout << estrutura[i % max_itens] << " ";
+        }
+        cout << "]\n";
+
+    }
